0d103f7351fbe470f14d1bd0b0ab96f3abe4cbed
PanosG <shiro.miyume@outlook.com>
2024-07-08 20:19:23 +0300

    Fetch quiz categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import { quizStatus } from "./interfaces/quizStatus";
+import { Category } from "./interfaces/category";
+import { getCategories } from "./util/api";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+  const [ quizStatus, setQuizStatus] = useState<quizStatus>('idle');
+  const [ categories, setCategories ] = useState<Category[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  useEffect(() => {
+    const startFetching = async () => {
+      const res = await getCategories();
+      console.log(res);
+      setCategories(res);
+    }
+
+    startFetching();
+  },[]);
+
+  return (
+    <>
+        <header>
+            <h1>Computer Science Quiz</h1>
+        </header>
+        <main>
+            {quizStatus === 'idle' && <section>
+                <header>
+                    <h3>Welcome to a quiz that's going to test your computer knowledge!</h3>
+                </header>
+                <div>
+                  <label htmlFor="categorySelect">Plese choose a category</label>
+                <select name="categorySelect" id="categorySelect" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+                  <option value="">----------</option>
+                  {categories && categories.map(cat => (
+                <option value={cat.name} key={cat.id}>{cat.name}</option>
+              ))}
+                </select>
+                </div>
+            </section>}
+        </main>
+    </>
+);}
 
 export default App;
