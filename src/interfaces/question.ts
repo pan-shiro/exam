5515e5c93661b818a2a59b93588944e65342874c
PanosG <shiro.miyume@outlook.com>
2024-07-08 20:33:03 +0300

    Create Question interface

---
+++ src/interfaces/question.ts
@@ -0,0 +1,9 @@
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    id: number;
+    multiple_correct_answers: string;
+    question: string;
+    answers: string[];
+}
