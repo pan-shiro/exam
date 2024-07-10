0ace8ff5863688bb1a84015347951276f0a595dd
PanosG <shiro.miyume@outlook.com>
2024-07-10 14:33:17 +0300

    Create optionRandomizer fn, Modify handleOperations

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { quizStatus } from "./interfaces/quizStatus";
 import { Category } from "./interfaces/category";
 import { getCategories, getQuestions } from "./util/api";
 import { Question } from "./interfaces/question";
-import SelectCategories from "./components/SelectCategories";
-import Instructions from "./components/Instructions";
 import GeneralHeader from "./components/GeneralHeader";
-import IntroHeader from "./components/IntroHeader";
 import StartingScreen from "./components/StartingScreen";
+import QuizForm from "./components/QuizForm";
+import ScorePage from "./components/ScorePage";
+import QuestionHeader from "./components/QuestionHeader";
 
 function App() {
   const [categories, setCategories] = useState<Category[]>([]);
@@ -23,6 +23,8 @@ function App() {
   const resultRef = useRef(false)
   const scoreRef = useRef(0);
 
+
+  //fetch and set categories
   useEffect(() => {
     const startFetching = async () => {
       const res = await getCategories();
@@ -33,18 +35,22 @@ function App() {
     startFetching();
   }, []);
 
+  //fetch and set category-specific questions
   useEffect(() => {
     const startFetching = async () => {
       const res = await getQuestions(selectedCategory);
       console.log(res);
       setQuestions(res);
     }
+
     if (selectedCategory === '') return;
+    if (quizStatus === 'finished') return;
     startFetching();
 
 
   }, [quizStatus]);
 
+  //create a timer
   useEffect(() => {
     if (quizStatus === 'begun') {
 
@@ -54,7 +60,7 @@ function App() {
             return t - 1;
           } else {
             handleOperations();
-            return 5;
+            return 30;
           }
         }));
         return () => clearInterval(intervalRef.current);
@@ -63,52 +69,83 @@ function App() {
     return () => clearInterval(intervalRef.current);
   }, [timer, quizStatus]);
 
+  //boolean checks used in Next button
+  const isCheckboxType = questions[currentIndex]?.multiple_correct_answers === 'true';
+  const checkboxCheck = multipleOptions.some(mo => mo);
 
+  //form submit function
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     handleOperations();
   }
-
-  const handleOperations = () => {
-    const { answers: currentOptions, correct_answer, multiple_correct_answers } = questions[currentIndex];
-
-    clearInterval(intervalRef.current);
-    if (multiple_correct_answers === "true") {
-      const { correct_answers } = questions[currentIndex];
-
-      const entries = Object.entries(correct_answers).filter((entry) =>
-        entry.includes("true")
-      );
-
-      const answers = entries
-        .map((entry) => entry[0])
-        .map((ans) => ans.slice(0, 8));
-
-      const result = answers.every((ans) => multipleOptions.includes(ans));
-      scoreRef.current = result ? scoreRef.current + 1 : scoreRef.current;
-    }
+  //give a random answer
+  const optionRandomizer = (currentOptions: string[]) => {
     const validKeys = Object.entries(currentOptions)
       .filter((ans) => !ans.includes(null))
       .map((entry) => entry[0]);
-    console.log(validKeys);
+    // console.log(validKeys);
+    //create a randomized index
     let randomIndex = Math.floor(Math.random() * validKeys.length);
     let randomAnswer = validKeys[randomIndex];
     console.log(`randomAnswer is ${randomAnswer}`);
+    return randomAnswer;
+  }
 
-    setSelectedOption(selectedOption === "" ? randomAnswer : selectedOption);
-    console.log(`correct_answer is ${correct_answer}`);
-    console.log(`selectedOption is ${selectedOption}`);
-    console.log(selectedOption === correct_answer);
+  //score operations based on input type, random answers 
+  const handleOperations = () => {
+    const { answers: currentOptions, correct_answer, } = questions[currentIndex];
+    clearInterval(intervalRef.current);
 
-    scoreRef.current = selectedOption === correct_answer ? scoreRef.current + 1 : scoreRef.current;
-    console.log("score is " + scoreRef.current)
+    //if options are of radio type
+    if (!isCheckboxType) {
+      console.log(`correct answer is ${correct_answer}`)
+      //if player hasn't selected anything, give a random answer
+      const randomAnswer = optionRandomizer(currentOptions);
+      setSelectedOption(selectedOption === '' ? randomAnswer : selectedOption);
+      console.log(`selected answer is ${selectedOption}`);
+      //adjust the score
+      if (selectedOption === '') {
+        scoreRef.current = randomAnswer === correct_answer ? scoreRef.current + 1 : scoreRef.current;
+      } else {
+        scoreRef.current = selectedOption === correct_answer ? scoreRef.current + 1 : scoreRef.current;
+      }
+      console.log("score is " + scoreRef.current)
+    } else {
+      //if options are of checkbox type
+      //if player has selected something
+      if (checkboxCheck) {
+        const { correct_answers } = questions[currentIndex];
+        //find correct entries
+        const entries = Object.entries(correct_answers).filter((entry) =>
+          entry.includes("true")
+        );
+        //get array of correct answers e.g. ['answer_a', 'answer_b']
+        const answers = entries
+          .map((entry) => entry[0])
+          .map((ans) => ans.slice(0, 8));
+        //check if answers given are equal to the correct answers
+        const result = answers.every((ans) => multipleOptions.includes(ans));
+        //adjust score accordingly
+        scoreRef.current = result ? scoreRef.current + 1 : scoreRef.current;
+      } else {
+        //if player hasn't selected anything
+        //question with multiple correct answers is never gonna be correct
+        //create a single random answer
+        //calculate score
+        const randomAnswer = optionRandomizer(currentOptions);
+        setSelectedOption(randomAnswer);
+        scoreRef.current = randomAnswer === correct_answer ? scoreRef.current + 1 : scoreRef.current;
+      }
+    }
 
-
+    //if currentIndex exists, reset timer, selectedOption and increase it
     if (currentIndex < questions.length - 1) {
       setTimer(30);
       setSelectedOption("");
       setCurrentIndex(ci => ci + 1);
-    } else {
+    } else
+    //quiz is over so estimate score values and cancel timer
+    {
       percentageRef.current = Math.round((scoreRef.current / questions.length) * 100);
       resultRef.current = percentageRef.current >= 50 ? true : false;
       setQuizStatus('finished')
@@ -118,19 +155,23 @@ function App() {
 
   const handleOptions = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, checked } = e.target;
+    //checkbox question
     if (questions[currentIndex].multiple_correct_answers === "true") {
+      //insert option to array
       if (checked) {
         setMultipleOptions((mo) => [...mo, value]);
       } else {
+        //checked option was unchecked, so remove it from array
         setMultipleOptions((mo) => mo.filter((n) => n !== value));
       }
     } else {
+      //radio question
       setSelectedOption(value);
     }
 
-    // console.log(multipleOptions);
   };
 
+  //reset variables
   const handleReset = () => {
     setQuizStatus('idle');
     setCurrentIndex(0);
@@ -144,61 +185,28 @@ function App() {
   }
 
 
+
+
   return (
     <div className="h-screen bg-slate-300">
       <GeneralHeader />
-      {/* <header className="text-3xl font-bold py-6 text-center">
-        <h1>Computer Science Quiz</h1>
-      </header> */}
       <main className="px-4 py-2 mt-4 w-full h-1/2 flex justify-center items-center">
-        {quizStatus === 'idle' && 
-        <StartingScreen selectedCategory={selectedCategory} categories={categories} onSelectOptionChange={(e) => setSelectedCategory(e.target.value)} onStartQuizBtnClick={() => setQuizStatus('begun')} />
+        {quizStatus === 'idle' &&
+          <StartingScreen selectedCategory={selectedCategory} categories={categories} onSelectOptionChange={(e: React.ChangeEvent<HTMLSelectElement>) => setSelectedCategory(e.target.value)} onStartQuizBtnClick={() => setQuizStatus('begun')} />
         }
         {quizStatus === 'begun' && questions.length > 0 ? <section className="w-1/2 min-h-72 p-2 mx-auto bg-slate-100 rounded-lg shadow-xl box-border">
-          <header className="flex justify-between items-center py-2 px-3">
-            <h5 className="text-sm">
-              Question <span className="font-semibold italic">{currentIndex + 1}</span> of <span className="font-semibold italic">{questions.length}</span>
-            </h5>
-            <div className="px-2 py-1 border border-dashed border-indigo-300 rounded-full w-7 h-7 flex justify-center items-center"><h5 className="text-sm font-semibold text-indigo-800">{timer}</h5></div>
-          </header>
-          <form className="px-3" onSubmit={handleSubmit}>
-            <p className="text-lg font-semibold mb-4">{questions && questions[currentIndex]?.question}</p>
-            <ul className="px-4">
-              {Object.values(questions[currentIndex].answers).filter((ans) => ans !== null).map((ans, i) => (
-                <li className="my-2 hover:bg-indigo-100 rounded-lg" key={ans}>
-                  {questions[currentIndex].multiple_correct_answers !== "true" ?
-                    <label className="align-middle font-mono w-full block">
-                      <input className="mx-4 align-middle" type="radio" name={currentIndex.toString()} value={Object.keys(questions[currentIndex].answers)[i]} onChange={handleOptions} />
-                      {ans}
-                    </label>
-                    :
-                    <label className="align-middle font-mono w-full block">
 
-                      <input className="mx-4 align-middle" type="checkbox" name={currentIndex.toString()} value={Object.keys(questions[currentIndex].answers)[i]} onChange={handleOptions} />
-                      {ans}
-                    </label>
-                  }
-                </li>
-              ))}
-            </ul>
-            <div className="flex justify-end">
-              <button className="rounded-lg bg-indigo-500 px-4 py-2 min-w-24 text-indigo-100 hover:bg-indigo-600 disabled:bg-indigo-300 disabled:cursor-not-allowed" type="submit" onClick={() => console.log(multipleOptions.length)} disabled={!selectedOption}>{currentIndex < questions.length - 1 ? "Next" : "Submit answers"} </button>
-            </div>
-          </form>
+          <QuestionHeader index={currentIndex} q={questions} timer={timer} />
+          <QuizForm onSubmitClick={handleSubmit} questions={questions} index={currentIndex} multOptions={multipleOptions} selectedOpt={selectedOption} onInputChange={handleOptions} checkboxCheck={checkboxCheck} isCheckboxType={isCheckboxType} />
         </section> : (quizStatus === 'begun' &&
           <div>
             <p className="italic text-2xl text-gray-500 text-center">Fetching questions...</p>
           </div>)
         }
 
-        {quizStatus === 'finished' && <section className="w-1/2 min-h-72 p-2 flex flex-col justify-center items-center gap-6 bg-slate-100 rounded-lg shadow-xl">
-          <ul className="text-xl space-y-3 w-3/4">
-            <li className="flex gap-2"><span className="font-semibold flex-1">Score: </span> <span className="font-mono text-2xl" style={{ color: resultRef.current ? 'green' : 'crimson' }}>{percentageRef.current}%</span></li>
-            <li className="flex gap-2"><span className="font-semibold flex-1">Total questions: </span><span className="font-mono text-2xl">{questions.length}</span></li>
-            <li className="flex gap-2"><span className="font-semibold flex-1">Correct answers: </span> <span className="font-mono text-2xl">{scoreRef.current}</span></li>
-          </ul>
-          <button className="rounded-lg bg-indigo-500 px-4 py-2 text-indigo-100 hover:bg-indigo-600" onClick={handleReset}>Try again</button>
-        </section>}
+        {quizStatus === 'finished' &&
+          <ScorePage resultRef={resultRef} percentageRef={percentageRef} questions={questions} scoreRef={scoreRef} onResetBtnClick={handleReset} />
+        }
       </main>
     </div>
   );
